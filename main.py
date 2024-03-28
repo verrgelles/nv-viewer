3e4eb3e3adcee1e044f21e1f01297a4e21bcc021
Simon Vergeles <verrgelles@gmail.com>
2024-03-28 14:41:50 +0300

    --

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from typing import List
-
-import matplotlib
-import pandas as pd
-import serial.tools.list_ports
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QMainWindow, QLineEdit, QPushButton, QComboBox, QLabel
-from scipy.interpolate import interp1d
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import random
 import socket
 import time
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import serial.tools.list_ports
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QMainWindow, QLineEdit, QPushButton, QLabel
+from matplotlib.backends.backend_qt import NavigationToolbar2QT
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
+from matplotlib.backends.backend_template import FigureCanvas
+from matplotlib.figure import Figure
+from scipy.interpolate import interp1d
+
 import odmrd_pb2
 
 
@@ -248,28 +253,24 @@ def get_color_gradient(c1, c2, n):
     rgb_colors = [((1 - mix) * c1_rgb + (mix * c2_rgb)) for mix in mix_pcts]
     return ["#" + "".join([format(int(round(val * 255)), "02x") for val in item]) for item in rgb_colors]
 
-
-def create_heatmap(data: pd.DataFrame):
-    cmap = matplotlib.colors.ListedColormap(get_color_gradient("#000000", "#ff0000", 2000))
-    hm = data.pivot(index='x', columns='y', values='v')
-    sns.heatmap(hm, cmap=cmap)
-    plt.show()
+class MplCanvas(FigureCanvasQTAgg):
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.com_port = None
+        self.com_port = find_com_port()
         self.scale_factor = 0.8
+        self.canvas = MplCanvas(self, width=5, height=5, dpi=100)
         self.x_angle = None
         self.y_angle = None
 
         self.setWindowTitle("\n")
-
-        com_port_selector = QLabel()
-        self.com_port = find_com_port()
-        com_port_selector.setText(f"COM-порт: {self.com_port} | Scale: {self.scale_factor}")
 
         self.input_x_angle = QLineEdit()
         self.input_x_angle.setPlaceholderText("∠x")
@@ -279,14 +280,41 @@ class MainWindow(QMainWindow):
         self.input_y_angle.setPlaceholderText("∠y")
         self.input_y_angle.textChanged.connect(self.y_angle_chosen)
 
+        self.open_map_button = QPushButton("Открыть карту")
+        self.open_map_button.clicked.connect(self.start_button_clicked)
+
         self.start_button = QPushButton("Начать картирование")
         self.start_button.clicked.connect(self.start_button_clicked)
 
+        def convert_csv_to_mpl(df: pd.DataFrame):
+            from math import isnan
+            t = []
+            for y in sorted(set(df['y']), reverse=True):
+                x_values = []
+                for x in (df.where(df['y'] == y))['v']:
+                    if not isnan(x):
+                        x_values.append(x)
+                t.append(x_values)
+            return sorted(set(df['x'])), sorted(set(df['y']), reverse=True), t
+
+        data = convert_csv_to_mpl(pd.read_csv('test_1.csv'))
+        cmap = matplotlib.colors.ListedColormap(get_color_gradient("#000000", "#ff0000", 2000))
+        xlabs = data[0]
+        ylabs = data[1]
+
+        self.canvas.axes.imshow(cmap=cmap, X=data[2])
+        self.canvas.axes.set_xticks(np.arange(len(xlabs)), labels = xlabs)
+        self.canvas.axes.set_yticks(np.arange(len(ylabs)), labels = ylabs)
+
+        toolbar = NavigationToolbar2QT(self.canvas, self)
+
         layout = QVBoxLayout()
         widgets = [
-            com_port_selector,
+            toolbar,
+            self.canvas,
             self.input_x_angle,
             self.input_y_angle,
+            self.open_map_button,
             self.start_button
         ]
 
@@ -303,6 +331,9 @@ class MainWindow(QMainWindow):
 
     def y_angle_chosen(self, i):
         self.y_angle = i
+
+    def open_map_button(self):
+        pass
 
     def start_button_clicked(self):
         scale_factor = float(self.scale_factor)
@@ -329,20 +360,48 @@ def main():
 
 def test_walk():
     ser = serial.Serial('COM5', 115200)
-    f = open('test.csv', 'w')
-    for x in (np.arange(0.150, 0.180, 0.001)):
+    ser.write(f"0.172|0.172F".encode())
+    f = open('test_1.csv', 'w')
+    for x in (np.arange(0.116, 0.134, 0.001)):
         x = str(x)[:5]
         if len(x) == 4:
             x = f'{x}0'
-        for y in (np.arange(0.150, 0.200, 0.001)):
+        for y in (np.arange(0.115, 0.149, 0.001)):
             y = str(y)[:5]
             if len(y) == 4:
                 y = f'{y}0'
             ser.write(f"{x}|{y}F".encode())
             time.sleep(1.0)
-            f.write(f'"({x}, {y})",{get_number_of_photons(1.0)}\n')
+            # f.write(f'"({x}, {y})",{get_number_of_photons(1.0)}\n')
     f.close()
 
 
+# По (116,115) правый верхний
+# По (134,149) левый нижний
+
+# центр 0.123, 0.130
+
+# x 0.120-0.126
+# y 0.126-0.134
+
+# 0.128, 0.126
+# x 0.125-0.131
+# y 0.122-0.130
 if __name__ == '__main__':
-    main()
+    """ser = serial.Serial('COM5', 115200)
+    f = open('test.csv', 'w')
+    for x in (np.arange(0.125, 0.131, 0.001)):
+        x = str(x)[:5]
+        if len(x) == 4:
+            x = f'{x}0'
+        for y in (np.arange(0.122, 0.130, 0.001)):
+            y = str(y)[:5]
+            if len(y) == 4:
+                y = f'{y}0'
+            ser.write(f"{x}|{y}F".encode())
+            time.sleep(1.0)
+            f.write(f'{x},{y},{get_number_of_photons(1.0)}\n')
+            print(f'{x},{y},{get_number_of_photons(1.0)}\n')
+    f.close()"""
+
+main()
